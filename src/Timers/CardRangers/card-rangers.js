dfa63a33aa86db96d6c1ddd13b5d8d1cfe62bf04
Gabe R <rodriguezgab1601@gmail.com>
2019-02-06 17:00:31 +0800

    Card Times added

--- src/Timers/CardRangers/card-rangers.js
+++ src/Timers/CardRangers/card-rangers.js
@@ -4,8 +4,34 @@ import { link } from "../home.json";
 import Countdown from "../countdown.js";
 import List from "../list.js";
 import CardTable from "./cardtable.js";
+import cardTimes from "./cardtimes.js";
+
+let i = 0;
 
 class CardRangers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerMin: 0,
+      timerHour: cardTimes[0].timerHour,
+      timerDay: cardTimes[0].timerDay,
+      id: cardTimes[0].id,
+      timerBg: "88:88:88"
+    };
+    this.countdowntime = this.countdowntime.bind(this);
+  }
+  countdowntime() {
+    i++;
+    if (i > 47) {
+      i = 0;
+    }
+    this.setState({
+      timerHour: cardTimes[i].timerHour,
+      timerDay: cardTimes[i].timerDay,
+      id: cardTimes[i].id
+    });
+    console.log(i);
+  }
   render() {
     return (
       <div id="cardRangers">
@@ -14,7 +40,13 @@ class CardRangers extends Component {
           timerColor={{ color: "#66b2ff" }}
           link={link}
         />
-        <Countdown timerDay={2} timerHour={17} timerBg="88:88:88" />
+        <Countdown
+          timerMin={this.state.timerMin}
+          timerHour={this.state.timerHour}
+          timerDay={this.state.timerDay}
+          timerBg={this.state.timerBg}
+          countdowntime={this.countdowntime}
+        />
         <List />
         <CardTable />
       </div>
---
+++ src/Timers/CardRangers/cardtimes.js
@@ -0,0 +1,244 @@
+const cardTimes = [
+  {
+    id: 0,
+    timerDay: 1,
+    timerHour: 1
+  },
+  {
+    id: 1,
+    timerDay: 1,
+    timerHour: 3
+  },
+  {
+    id: 2,
+    timerDay: 1,
+    timerHour: 12
+  },
+  {
+    id: 3,
+    timerDay: 1,
+    timerHour: 14
+  },
+  {
+    id: 4,
+    timerDay: 1,
+    timerHour: 17
+  },
+  {
+    id: 5,
+    timerDay: 1,
+    timerHour: 19
+  },
+  {
+    id: 6,
+    timerDay: 1,
+    timerHour: 21
+  },
+  {
+    id: 7,
+    timerDay: 1,
+    timerHour: 23
+  },
+  {
+    id: 8,
+    timerDay: 2,
+    timerHour: 1
+  },
+  {
+    id: 9,
+    timerDay: 2,
+    timerHour: 3
+  },
+  {
+    id: 10,
+    timerDay: 2,
+    timerHour: 12
+  },
+  {
+    id: 11,
+    timerDay: 2,
+    timerHour: 14
+  },
+  {
+    id: 12,
+    timerDay: 2,
+    timerHour: 17
+  },
+  {
+    id: 13,
+    timerDay: 2,
+    timerHour: 19
+  },
+  {
+    id: 14,
+    timerDay: 2,
+    timerHour: 21
+  },
+  {
+    id: 15,
+    timerDay: 2,
+    timerHour: 23
+  },
+  {
+    id: 16,
+    timerDay: 3,
+    timerHour: 1
+  },
+  {
+    id: 17,
+    timerDay: 3,
+    timerHour: 3
+  },
+  {
+    id: 18,
+    timerDay: 3,
+    timerHour: 12
+  },
+  {
+    id: 19,
+    timerDay: 3,
+    timerHour: 14
+  },
+  {
+    id: 20,
+    timerDay: 3,
+    timerHour: 17
+  },
+  {
+    id: 21,
+    timerDay: 3,
+    timerHour: 19
+  },
+  {
+    id: 22,
+    timerDay: 3,
+    timerHour: 21
+  },
+  {
+    id: 23,
+    timerDay: 3,
+    timerHour: 23
+  },
+  {
+    id: 24,
+    timerDay: 4,
+    timerHour: 1
+  },
+  {
+    id: 25,
+    timerDay: 4,
+    timerHour: 3
+  },
+  {
+    id: 26,
+    timerDay: 4,
+    timerHour: 12
+  },
+  {
+    id: 27,
+    timerDay: 4,
+    timerHour: 14
+  },
+  {
+    id: 28,
+    timerDay: 4,
+    timerHour: 17
+  },
+  {
+    id: 29,
+    timerDay: 4,
+    timerHour: 19
+  },
+  {
+    id: 30,
+    timerDay: 4,
+    timerHour: 21
+  },
+  {
+    id: 31,
+    timerDay: 4,
+    timerHour: 23
+  },
+  {
+    id: 32,
+    timerDay: 5,
+    timerHour: 1
+  },
+  {
+    id: 33,
+    timerDay: 5,
+    timerHour: 3
+  },
+  {
+    id: 34,
+    timerDay: 5,
+    timerHour: 12
+  },
+  {
+    id: 35,
+    timerDay: 5,
+    timerHour: 14
+  },
+  {
+    id: 36,
+    timerDay: 5,
+    timerHour: 17
+  },
+  {
+    id: 37,
+    timerDay: 5,
+    timerHour: 19
+  },
+  {
+    id: 38,
+    timerDay: 5,
+    timerHour: 21
+  },
+  {
+    id: 39,
+    timerDay: 5,
+    timerHour: 23
+  },
+  {
+    id: 40,
+    timerDay: 6,
+    timerHour: 17
+  },
+  {
+    id: 41,
+    timerDay: 6,
+    timerHour: 19
+  },
+  {
+    id: 42,
+    timerDay: 6,
+    timerHour: 21
+  },
+  {
+    id: 43,
+    timerDay: 6,
+    timerHour: 23
+  },
+  {
+    id: 44,
+    timerDay: 1,
+    timerHour: 17
+  },
+  {
+    id: 45,
+    timerDay: 1,
+    timerHour: 19
+  },
+  {
+    id: 46,
+    timerDay: 1,
+    timerHour: 21
+  },
+  {
+    id: 47,
+    timerDay: 1,
+    timerHour: 23
+  }
+];
+
+export default cardTimes;
