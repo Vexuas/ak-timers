5c948263ca0b88398bc6b0a552a3426193b61162
Gabe R <rodriguezgab1601@gmail.com>
2019-02-06 17:00:40 +0800

    Raid Times added

---
+++ src/Timers/Raids/raidtimes.js
@@ -0,0 +1,123 @@
+const raidTimes = [
+  {
+    id: 0,
+    timerDay: 4,
+    timerHour: 12
+  },
+  {
+    id: 1,
+    timerDay: 4,
+    timerHour: 12
+  },
+  {
+    id: 2,
+    timerDay: 4,
+    timerHour: 15
+  },
+  {
+    id: 3,
+    timerDay: 4,
+    timerHour: 17
+  },
+  {
+    id: 4,
+    timerDay: 4,
+    timerHour: 20
+  },
+  {
+    id: 5,
+    timerDay: 4,
+    timerHour: 22
+  },
+  {
+    id: 6,
+    timerDay: 5,
+    timerHour: 10
+  },
+  {
+    id: 7,
+    timerDay: 5,
+    timerHour: 12
+  },
+  {
+    id: 8,
+    timerDay: 5,
+    timerHour: 15
+  },
+  {
+    id: 9,
+    timerDay: 5,
+    timerHour: 17
+  },
+  {
+    id: 10,
+    timerDay: 5,
+    timerHour: 20
+  },
+  {
+    id: 11,
+    timerDay: 5,
+    timerHour: 22
+  },
+  {
+    id: 12,
+    timerDay: 6,
+    timerHour: 10
+  },
+  {
+    id: 13,
+    timerDay: 6,
+    timerHour: 12
+  },
+  {
+    id: 14,
+    timerDay: 6,
+    timerHour: 15
+  },
+  {
+    id: 15,
+    timerDay: 6,
+    timerHour: 17
+  },
+  {
+    id: 16,
+    timerDay: 6,
+    timerHour: 20
+  },
+  {
+    id: 17,
+    timerDay: 6,
+    timerHour: 22
+  },
+  {
+    id: 18,
+    timerDay: 0,
+    timerHour: 10
+  },
+  {
+    id: 19,
+    timerDay: 0,
+    timerHour: 12
+  },
+  {
+    id: 20,
+    timerDay: 0,
+    timerHour: 15
+  },
+  {
+    id: 21,
+    timerDay: 0,
+    timerHour: 17
+  },
+  {
+    id: 22,
+    timerDay: 0,
+    timerHour: 20
+  },
+  {
+    id: 23,
+    timerDay: 0,
+    timerHour: 22
+  }
+];
+export default raidTimes;
--- src/Timers/Raids/skytower.js
+++ src/Timers/Raids/skytower.js
@@ -4,8 +4,35 @@ import { link } from "../home.json";
 import Countdown from "../countdown.js";
 import RaidTable from "./raidtable.js";
 import List from "../list.js";
+import raidTimes from "./raidtimes.js";
+
+let i = 0;
 
 class Skytower extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerMin: 0,
+      timerHour: raidTimes[0].timerHour,
+      timerDay: raidTimes[0].timerDay,
+      id: raidTimes[0].id,
+      timerBg: "88:88:88"
+    };
+    this.countdowntime = this.countdowntime.bind(this);
+  }
+  countdowntime() {
+    i++;
+    if (i > 23) {
+      i = 0;
+    }
+    this.setState({
+      timerHour: raidTimes[i].timerHour,
+      timerDay: raidTimes[i].timerDay,
+      id: raidTimes[i].id
+    });
+    console.log(i);
+  }
+
   render() {
     return (
       <div id="skyTower">
@@ -14,7 +41,13 @@ class Skytower extends Component {
           timerColor={{ color: "#66b2ff" }}
           link={link}
         />
-        <Countdown timerDay={4} timerHour={10} timerBg="888:88:88" />
+        <Countdown
+          timerMin={this.state.timerMin}
+          timerHour={this.state.timerHour}
+          timerDay={this.state.timerDay}
+          timerBg={this.state.timerBg}
+          countdowntime={this.countdowntime}
+        />
         <List />
         <RaidTable />
       </div>
