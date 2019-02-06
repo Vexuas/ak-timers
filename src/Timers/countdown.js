b8fd4ea9c75d02d780d515207e851c89fb2fa013
Gabe R <rodriguezgab1601@gmail.com>
2019-02-06 17:00:12 +0800

    New countdown codes

--- src/Timers/countdown.js
+++ src/Timers/countdown.js
@@ -14,20 +14,21 @@ class Countdown extends Component {
   }
   componentDidMount() {
     setInterval(() => {
-      const d = new Date();
+      const current = new Date();
       const day = this.props.timerDay; // 0-6, Thursday
+      //Server Time
+      const utc = current.getTime() + current.getTimezoneOffset() * 60000;
+      const offset = -5;
+      const gameTime = utc + 3600000 * offset;
+      const d = new Date(gameTime);
       const countertime = new Date(
         d.getFullYear(),
         d.getMonth(),
         d.getDate(),
         this.props.timerHour,
-        0,
+        this.props.timerMin,
         0
       ); //10 out of 24 hours = 10am
-      //Server Time
-      const utc = d.getTime() + d.getTimezoneOffset() * 60000;
-      const offset = -5;
-      const gameTime = utc + 3600000 * offset;
       //Countdown Time
       const countTime =
         countertime.getTime() + currentDay * 24 * 60 * 60 * 1000; //adding day to timer
@@ -38,27 +39,27 @@ class Countdown extends Component {
         currentDay = day - d.getDay() - 1; //after countdown time
       }
       if (currentDay < 0) {
-        currentDay += 7; //switch to next week
+        currentDay = day - d.getDay();
       }
       if (diff <= 0) {
-        diff += 86400 * 1000 * 7;
+        this.props.countdowntime();
       }
-
+      //Countdown timer
       const hours = Math.floor(
         (diff % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60)
       );
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+      const hour = ("000" + hours).substr(-2);
       const minute = ("00" + minutes).substr(-2);
       const second = ("00" + seconds).substr(-2);
       this.setState({
-        hour: hours,
+        hour: hour,
         minute: minute,
         second: second
       });
     });
   }
-
   render() {
     return (
       <div className="timerTemplate">
